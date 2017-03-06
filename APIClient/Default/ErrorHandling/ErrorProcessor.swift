344987016d05e535bcd48b90e1c03d52881771ee
Roman Kyrylenko <roman.kyrylenko@gmail.com>
2017-03-06 23:35:13 +0200

    [Fix] added public constructor to `ErrorProcessor`

--- APIClient/Default/ErrorHandling/ErrorProcessor.swift
+++ APIClient/Default/ErrorHandling/ErrorProcessor.swift
@@ -10,6 +10,9 @@ public struct NetworkErrorProcessor: ErrorProcessing {
     
     private let deserializer = JSONDeserializer()
     
+    public init() {
+    }
+    
     public func processError(using response: APIClient.HTTPResponse) -> Error? {
         if let dictionary = (try? deserializer.deserialize(response.0, data: response.1)) as? [String: AnyObject] {
             return NetworkError(statusCode: response.0.statusCode, responseDictionary: dictionary)
