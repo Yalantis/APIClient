f02dcd2fa185ca261772fcbe32f3dffcfbf2f623
rnkyr <pamuk635@gmail.com>
2019-04-22 16:16:10 +0300

    [update] dependencies, swift version

--- APIClient/Alamofire/APIRequest+Alamofire.swift
+++ APIClient/Alamofire/APIRequest+Alamofire.swift
@@ -3,11 +3,11 @@ import Alamofire
 
 public extension APIRequest {
 
-    public var alamofireMethod: HTTPMethod {
+    var alamofireMethod: HTTPMethod {
         return HTTPMethod(rawValue: method.rawValue.uppercased()) ?? .get
     }
 
-    public var alamofireEncoding: ParameterEncoding {
+    var alamofireEncoding: ParameterEncoding {
         return encoding as? ParameterEncoding ?? URLEncoding.default
     }
     
